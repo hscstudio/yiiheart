9e641e72ff2ea034f181f693725ac8c5cb77f714
Hafid Mukhlasin <milisstudio@gmail.com>
2014-05-14 13:27:37 +0700

    DELETE TO UPDATE 1.1

--- heart/protected/views/layouts/column1.php
+++
@@ -1,7 +0,0 @@
-<?php /* @var $this Controller */ ?>
-<?php $this->beginContent('//layouts/main'); ?>
-<div id="content">
-	<?php echo $content; ?>
-</div><!-- content -->
-<?php $this->endContent(); ?>
-<div><hr></div>
